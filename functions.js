4d3b5194b57e87c2d46f03c3ae698f7e8eb80081
Alexandra Chicinas <alexandraschicinas@yahoo.com>
2021-01-14 19:46:09 +0200

    improved functions I

--- functions.js
+++ functions.js
@@ -1,15 +1,35 @@
-function showSkillsPage() {
+function skills() {
     var page = document.getElementById('skills');
     page.style.display = '';
+}
+
+function hideHome() {
     var home = document.getElementById('home');
     home.style.display = 'none';
+}
+
+function hideLanguages () {
     var languages = document.getElementById('languages');
     languages.style.display = 'none';
+}
+
+function showSkillsPage() {
+    // var page = document.getElementById('skills');
+    // page.style.display = '';
+    skills();
+
+    hideHome();
+    hideLanguages();
+    // var home = document.getElementById('home');
+    // home.style.display = 'none';
+    // var languages = document.getElementById('languages');
+    // languages.style.display = 'none';
 }
 
 function showLanguagesPage() {
     var languages = document.getElementById('languages');
     languages.style.display = '';
+
     var home = document.getElementById('home');
     home.style.display = 'none';
     var page = document.getElementById('skills');
@@ -21,6 +41,7 @@ function showLanguagesPage() {
 function showProjectsPage() {
     var project = document.getElementById("projects");
     project.style.display = '';
+
     var home = document.getElementById('home');
     home.style.display = 'none';
     var languages = document.getElementById('languages');
@@ -32,6 +53,7 @@ function showProjectsPage() {
 function showPageHome() {
     var home = document.getElementById('home');
     home.style.display = '';
+
     var languages = document.getElementById('languages');
     languages.style.display = 'none';
     var page = document.getElementById('skills');
